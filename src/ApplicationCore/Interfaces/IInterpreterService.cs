6555619625b5ff92631b4569f713d0c56712860a
Tomasz Plewniak <123762965+tomasz-plewniak@users.noreply.github.com>
2024-03-26 17:09:07 +0100

    Feature/implementation (#4)
    * Add env tests.
    
    * Add implementation.
    
    * Add some example of testing.

---
+++ src/ApplicationCore/Interfaces/IInterpreterService.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationCore.Interfaces;
+
+public interface IInterpreterService
+{
+    object Atom(string token);
+
+    object Eval(object ast);
+    
+    object Parse(List<string> tokens);
+    
+    List<string> Tokenize(string input);
+
+    object RunInterpreter(string input);
+}
---
+++ tests/UnitTests/InterpreterServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore;
+using Environment = ApplicationCore.Environment;
+
+namespace UnitTests;
+
+public class InterpreterServiceTests
+{
+    private InterpreterService _interpreterService;
+    
+    public InterpreterServiceTests()
+    {
+        _interpreterService = new InterpreterService(new Environment());
+    }
+    
+    [Fact]
+    public void Tokenize_ShouldSplitExpressionIntoTokens()
+    {
+        var input = "(define a 10)";
+        var expectedTokens = new List<string> { "(", "define", "a", "10", ")" };
+
+       var tokens = _interpreterService.Tokenize(input);
+
+       tokens.Should().BeEquivalentTo(expectedTokens, options => options.WithStrictOrdering());
+    }
+}
