f4076006fccf2ab0849b4b7ce13c4a8889b65dc2
Tomasz Plewniak <123762965+tomasz-plewniak@users.noreply.github.com>
2024-03-26 16:24:33 +0100

    Feature/create environment (#3)
    * Ignore Rider files.
    
    * Add blank solution.
    
    * Base setup of the projects.
    
    * Implement environment.
    
    (cherry picked from commit 6f0b13c734e50c9c185eda4c2552b104ef3d9730)

---
+++ src/ApplicationCore/Exceptions/UndefinedSymbolException.cs
@@ -0,0 +1,18 @@
+﻿namespace ApplicationCore.Exceptions;
+
+public class UndefinedSymbolException : Exception
+{
+    public UndefinedSymbolException()
+    {
+    }
+
+    public UndefinedSymbolException(string message)
+        : base(message)
+    {
+    }
+
+    public UndefinedSymbolException(string message, Exception inner)
+        : base(message, inner)
+    {
+    } 
+}
--- src/LispInterpreter/Program.cs
+++ src/LispInterpreter/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+using ApplicationCore.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Environment = ApplicationCore.Environment;
 
-Console.WriteLine("Hello, World!");
+
+var serviceProvider = new ServiceCollection()
+    .AddLogging(builder => builder.AddConsole())
+    .AddSingleton<IEnvironment, Environment>()
+    .BuildServiceProvider();
+
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+ILogger logger = factory.CreateLogger<Program>();
+
+logger.LogInformation("Starting application");
+
+var environment = serviceProvider.GetService<IEnvironment>();
+
+environment.Set("test", "hehe");
+
+var result = environment.Get("test");
+
+logger.LogInformation("End");
